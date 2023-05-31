4cb6da4b079d77f31ce1a79380ff658d3272481f
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-05-31 03:54:13 +0100

    Adding calculator design

---
+++ src/components/buttons.js
@@ -0,0 +1,76 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function Buttons() {
+  return (
+    <div className="buttons">
+      <button className="button" type="button">AC</button>
+      <button className="button" type="button">+/-</button>
+      <button className="button" type="button">%</button>
+      <button className="button operator" type="button">/</button>
+      <button className="button" type="button">7</button>
+      <button className="button" type="button">8</button>
+      <button className="button" type="button">9</button>
+      <button className="button operator" type="button">X</button>
+      <button className="button" type="button">4</button>
+      <button className="button" type="button">5</button>
+      <button className="button" type="button">6</button>
+      <button className="button operator" type="button">-</button>
+      <button className="button" type="button">1</button>
+      <button className="button" type="button">2</button>
+      <button className="button" type="button">3</button>
+      <button className="button operator" type="button">+</button>
+      <button className="button ziro" type="button">0</button>
+      <button className="button" type="button">.</button>
+      <button className="button operator" type="button">=</button>
+      {/* <button className="button equal" type="button">=</button> */}
+      {/* <div className="row">
+        <button className="button" type="button">AC</button>
+        <button className="button" type="button">+/-</button>
+        <button className="button" type="button">%</button>
+        <button className="button operator" type="button">/</button>
+        <button className="button" type="button">7</button>
+        <button className="button" type="button">8</button>
+        <button className="button" type="button">9</button>
+        <button className="button operator" type="button">X</button>
+      </div>
+      <div className="row">
+        <button className="button" type="button">7</button>
+        <button className="button" type="button">8</button>
+        <button className="button" type="button">9</button>
+        <button className="button operator" type="button">X</button>
+      </div>
+      <div className="row">
+        <button className="button" type="button">4</button>
+        <button className="button" type="button">5</button>
+        <button className="button" type="button">6</button>
+        <button className="button operator" type="button">-</button>
+      </div>
+      <div className="row">
+        <button className="button" type="button">1</button>
+        <button className="button" type="button">2</button>
+        <button className="button" type="button">3</button>
+        <button className="button operator" type="button">+</button>
+      </div>
+      <div className="row">
+        <button className="button ziro" type="button">0</button>
+        <button className="button" type="button">.</button>
+        <button className="button operator" type="button">=</button>
+        <button className="button equal" type="button">=</button>
+      </div> */}
+    </div>
+  );
+}
+
+// Button.defaultProps = {
+//   content: 'random',
+// };
+
+// Button.propTypes = {
+//   content: PropTypes.oneOfType([
+//     PropTypes.string,
+//     PropTypes.number,
+//   ]),
+// };
+
+export default Buttons;
